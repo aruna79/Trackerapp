76e7a9aad7ac34c90fae89c54d823394f8e8b097
Aruna <aruna.thungathurthi@gmail.com>
2018-07-15 15:01:28 -0700

    Login activity

---
+++ app/src/main/java/com/example/arunathungathurthi/tracker/Login.java
@@ -0,0 +1,26 @@
+package com.example.arunathungathurthi.tracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+    EditText EditNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        EditNumber=(EditText)findViewById(R.id.EditNumber);
+    }
+
+    public void BuNext(View view) {
+
+        GlobalInfo.PhoneNumber=GlobalInfo.FormatPhoneNumber(EditNumber.getText().toString());
+        GlobalInfo.UpdatesInfo(GlobalInfo.PhoneNumber);
+        finish();
+        Intent intent=new Intent(this, MyTrackers.class);
+        startActivity(intent);
+    }
+}
